25588554d3c25a605a28a1e4ff66ede1e5b729b8
chk1226 <chk1226@hotmail.com>
2017-05-05 03:43:52 +0900

    no message

---
+++ MyRender/Source/Debug/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRender.Debug
+{
+    class Log
+    {
+        public static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
